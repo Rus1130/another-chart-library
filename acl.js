f04ea691b31e11d1ee7aa2d0a343e84dcfe39042
Rus1130 <arirogozi@gmail.com>
2023-05-22 19:47:47 -0400

    commiyt

--- acl.js
+++ acl.js
@@ -4,7 +4,7 @@ import { SVG, extend as SVGextend, Element as SVGElement } from 'https://cdnjs.c
 /*
 Data Format:
 
-[x_axis_label, y_axis_label, [...x_axis_data], [...y_axis_data]]
+[x_axis_label, y_axis_label, ymin, ymax, ystep, [...x_axis_data], [...y_axis_data]]
 
 
 
@@ -16,12 +16,26 @@ class BarChart {
         let xAxisLabel = data[0];
         let yAxisLabel = data[1];
 
-        let xText = draw.text(xAxisLabel).font({ size: 12, family: 'Helvetica' })
-        xText.move(options.width/2 - xText.bbox().width/2, options.height - (options.height / options.precision) / 2 - 5)
+        let ymin = data[2];
+        let ymax = data[3];
+        let ystep = data[4];
 
-        let yText = draw.text(yAxisLabel).font({ size: 12, family: 'Helvetica' })
-        yText.move((options.width / options.precision / 2 - yText.bbox().width / 2) - 5, options.height / 2 - yText.bbox().height / 2)
-        yText.rotate(-90, options.width / options.precision / 2, options.height / 2)
+        // put a label between xAxisLine2 and the bottom of the chart
+
+        let xLine = Chart.measureLines.xAxisLine;
+        let yLine = Chart.measureLines.yAxisLine;
+
+        let xCenter = Chart.measureLines.xCenterLine;
+        let yCenter = Chart.measureLines.yCenterLine;
+
+        let xLabel = draw.text(xAxisLabel).font({ family: 'Helvetica', size: 16 })
+        .cx(xCenter.attr('x1'))
+        .cy(options.height - (options.height - xLine.attr('y1')) / 2 * 0.75)
+
+        let yLabel = draw.text(yAxisLabel).font({ family: 'Helvetica', size: 16 })
+        .cx(yLine.attr('x2') / 2 * 0.75)
+        .cy(yCenter.attr('y1'))
+        .rotate(-90)
 
     }
 }
@@ -38,6 +52,11 @@ export class Chart {
     static SVGData = ''
     static type = ''
     static draw = null;
+    static measureLines = []
+
+    static fullScreen = false;
+    static width = 500;
+    static height = 500;
 
     static options = null;
 
@@ -50,27 +69,36 @@ export class Chart {
         options.width = options.width || 500;
         options.height = options.height || 500;
         options.fullscreen = options.fullscreen || false;
-        options.precision = 9;
+        options.precision = 6;
 
         Chart.options = options;
-
-        if(options.fullscreen) {
-            options.width = element.offsetWidth;
-            options.height = element.offsetHeight;
-            Chart.options = options;
-
-            window.addEventListener('resize', () => {
-                options.width = element.offsetWidth;
-                options.height = element.offsetHeight;
-                Chart.options = options;
-            })
-        }
 
         this.draw = SVG().addTo(element).size(options.width, options.height);
         options.draw = this.draw
 
-        let xLine1 = this.draw.line(5, options.height - options.height/options.precision, options.width - 5, options.height - options.height/options.precision).stroke({ width: 1, color: '#000' });
+        if(this.type == 'bar') {
+            let h = options.height;
+            let w = options.width;
+            let p = options.precision;
 
+            let xAxisLine = this.draw.line(5, h - h / p, w - 5, h - h / p).stroke({ width: 1, color: '#000' });
+            let yAxisLine = this.draw.line(w / p, 5, w / p, h - 5).stroke({ width: 1, color: '#000' });
+
+            let rect = this.draw.rect(xAxisLine.attr('x2') - yAxisLine.attr('x1') - 5, xAxisLine.attr('y1') - 10)
+            .move(yAxisLine.attr('x1') + 5, yAxisLine.attr('y1')).fill('none')
+
+            // create a line going down the middle of rect
+            let xCenterLine = this.draw.line(rect.attr('x') + rect.attr('width') / 2, rect.attr('y'), rect.attr('x') + rect.attr('width') / 2, h - 5).stroke('none');
+            let yCenterLine = this.draw.line(5, rect.attr('y') + rect.attr('height') / 2, w - 5, rect.attr('y') + rect.attr('height') / 2).stroke('none');
+
+            Chart.measureLines = {
+                xAxisLine: xAxisLine,
+                yAxisLine: yAxisLine,
+                rect: rect,
+                xCenterLine: xCenterLine,
+                yCenterLine: yCenterLine
+            }
+        }
 
         return this;
     }
